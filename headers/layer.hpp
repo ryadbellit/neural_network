e5e2f1e9018870e1b42e2b5aa99c9563fbad16c2
ryadbellit <ryad.bellit@gmail.com>
2026-01-15 02:10:50 -0500

    Avancer forward propagation de layer.cpp

--- headers/layer.hpp
+++ headers/layer.hpp
@@ -1,12 +1,44 @@
 #include <matrix.hpp>
 
+enum class Activation { RELU, SOFTMAX, NONE };
+
 class Layer {
 public:
+    
+    Layer(int inputSize, int outputSize, Activation act = Activation::NONE) : 
+        weights(inputSize, outputSize), biases(1, outputSize), 
+        lastInput(0, 0), lastOutput(0, 0), type(act) {
+        
+        double scale;
+        switch (type) {
+            case Activation::RELU:
+                scale = std::sqrt(2.0 / inputSize); // Xavier initialization
+                break;
+            case Activation::SOFTMAX:
+                scale = std::sqrt(2.0 / inputSize + outputSize); // He initialization
+                break;
+            case Activation::NONE:
+                scale = 0.5;
+                break;
+        }
 
-    Matrix weights;
-    Matrix biases;
-    Matrix lastInput;
+        weights.fillRandom(-scale, scale);
+        biases.fill(0.01);
+    }
 
     Matrix forward(const Matrix& input);
     Matrix backward(const Matrix& outputGradient, const double learningRate);
+    Matrix getWeights() const { return weights; }
+    Matrix getBiases() const { return biases; }
+private:
+    Matrix weights;
+    Matrix biases;
+
+    Matrix lastInput;
+    Matrix lastOutput;
+
+    Activation type;
+
+    Matrix applyActivation(const Matrix& matrix);
+    Matrix applyActivationDerivative(const Matrix& matrix);
 };
--- src/layer.cpp
+++ src/layer.cpp
@@ -1,2 +1,48 @@
 #include <layer.hpp>
 
+Matrix Layer::forward(const Matrix& input) {
+
+
+    auto firstProduct = input * weights;
+    if (!firstProduct) {
+        throw std::runtime_error("Matrix sizes are not compatible");
+    }
+    
+    auto z = firstProduct.value() + biases;
+
+    if (!z) {
+        throw std::runtime_error("Biases size is not compatible");
+    }
+
+    Matrix result(z.value().getRows(), z.value().getColumns());
+
+    switch (type) {
+        case Activation::RELU:
+            result = z.value().relu();
+            break;
+        case Activation::SOFTMAX:
+            result = z.value().stable_softmax();
+            break;
+        case Activation::NONE:
+            result = z.value();
+            break;
+    }
+
+    this->lastInput = input;
+    this->lastOutput = result;
+
+    return result;
+
+}
+
+Matrix Layer::backward(const Matrix& outputGradient, const double learningRate) {
+
+}
+
+Matrix Layer::applyActivation(const Matrix& matrix) {
+
+}
+
+Matrix Layer::applyActivationDerivative(const Matrix& matrix) {
+
+}
